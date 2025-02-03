cf3b3a514ab76da0341960afe2678b4c02aba8a4
swathynambiar <61445649+swathynambiar@users.noreply.github.com>
2025-02-03 17:47:14 -0500

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -8,7 +8,7 @@ last_name VARCHAR(20),
 primary_skill VARCHAR(20),
 location VARCHAR(20));
 
-INSERT INTO employee VALUES ('1','Amanda','Williams','Smile','local');
+INSERT INTO employee VALUES ('1','Amanda','Williams','Smile','locals');
 INSERT INTO employee VALUES ('1','Alan','Williams','Empathy','alien');
 SELECT * FROM employee;
 
